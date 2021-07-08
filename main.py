44ba3314b07bfbc9d769637b9247b2424afd094a
Anne Guichard <anne.guichard@irisa.fr>
2021-07-08 09:09:25 +0200

    docs(main): update version of MTG-Link to v2.1.0

--- main.py
+++ main.py
@@ -37,7 +37,7 @@ from Bio import SeqIO
 # Arg parser
 #----------------------------------------------------
 
-MTGLINK_VERSION = "2.0.0"
+MTGLINK_VERSION = "2.1.0"
 
 parser = argparse.ArgumentParser(prog="mtglink.py", \
                                 formatter_class=argparse.RawTextHelpFormatter, \
