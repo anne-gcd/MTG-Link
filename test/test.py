fb8cd2ad55ed6d30e716088e29c0509545f72bdd
Anne Guichard <anne.guichard@irisa.fr>
2022-09-02 08:39:52 +0200

    fix(test/test.py): update the 'test.py' script to test several features of MTG-Link

--- test/test.py
+++ test/test.py
@@ -74,9 +74,9 @@ with open(commandOut) as out:
     else:
         print("MTG-Link DBG w/o ref: Fail")
 
-# subprocess.run("rm LOGMTGLinkDBGRunInto", shell=True)
-# subprocess.run("rm ERRLOGMTGLinkDBGRunInto", shell=True)
-# subprocess.run("rm -rf test_run/gapsIntoScaffolds/results_MTGLink_DBG", shell=True)
+subprocess.run("rm LOGMTGLinkDBGRunInto", shell=True)
+subprocess.run("rm ERRLOGMTGLinkDBGRunInto", shell=True)
+subprocess.run("rm -rf test_run/gapsIntoScaffolds/results_MTGLink_DBG", shell=True)
 
 
 # ### MTG-Link IRO without reference
@@ -102,20 +102,29 @@ with open(commandOut) as out:
 # subprocess.run("rm -rf test_run/gapsIntoScaffolds/results_MTGLink_IRO", shell=True)
 
 
-# ### MTG-Link DBG with reference
-# /usr/bin/time -v ../mtglink.py DBG -gfa test_run/gapsIntoScaffolds/test.gfa -bam test_run/gapsIntoScaffolds/test.bam -fastq test_run/gapsIntoScaffolds/reads.sorted.fastq.gz -index test_run/gapsIntoScaffolds/barcoded.bci -k 61 51 41 31 21 -t 3 -refDir test_run/gapsIntoScaffolds -out test_run/gapsIntoScaffolds/results_MTGLink_DBG_withRef > LOGMTGLinkDBGRunIntoRef 2> ERRLOGMTGLinkDBGRunIntoRef  
-# val=$(awk '/\t\* 68_0-222132-L+:68_223132-445265-R+\n\t\t\* fwd\t2000 bp\tA\n\t\t\* rev\t2000 bp\tA\n\t\* 26939_0-464805-L+:26939_465805-930610-R+\n\t\t\* fwd\t2004 bp\tB\n\t\t\* rev\t2004 bp\tB/' LOGMTGLinkDBGRunIntoRef)
-# val1=$(awk '/The gap 27292_0-183358-L+_27292_184358-367715-R+ was not successfully gap-filled/' LOGMTGLinkDBGRunIntoRef)
-# if [ -z "$val" ] || [ -z "$val1" ]
-# then
-#     echo "MTG-Link DBG w/ ref: Fail"
-# else
-#     echo "MTG-Link DBG w/ ref: Pass"
-# fi
+### MTG-Link DBG with reference
+command = ["/usr/bin/time", "-v", "../mtglink.py", "DBG", \
+            "-gfa", "test_run/gapsIntoScaffolds/test.gfa", "-bam", "test_run/gapsIntoScaffolds/test.bam", \
+            "-fastq", "test_run/gapsIntoScaffolds/reads.sorted.fastq.gz", "-index", "test_run/gapsIntoScaffolds/barcoded.bci", \
+            "-k", "61", "51", "41", "31", "21", "-t", "4", \
+            "-refDir", "test_run/gapsIntoScaffolds/", \
+            "-out", "test_run/gapsIntoScaffolds/results_MTGLink_DBG_withRef"]
+commandOut = "LOGMTGLinkDBGRunIntoRef"
+commandLog = "ERRLOGMTGLinkDBGRunIntoRef"
+with open(commandOut, "a") as out, open(commandLog, "a") as log:
+    subprocess.run(command, stdout=out, stderr=log)
 
-# rm LOGMTGLinkDBGRunIntoRef
-# rm ERRLOGMTGLinkDBGRunIntoRef
-# rm -rf test_run/gapsIntoScaffolds/results_MTGLink_DBG_withRef
+expectedString = "In total, 3 targets were successfully assembled:\n\t* 68_0-222132-L+:68_223132-445265-R+\n\t\t* fwd_1/1.k61\t2000 bp\tA\n\t\t* rev_1/1.k61\t2000 bp\tA\n\t* 26939_0-464805-L+:26939_465805-930610-R+\n\t\t* fwd_1/1.k51\t2004 bp\tB\n\t\t* rev_1/1.k51\t2004 bp\tB\n\t* 27358_0-171884-L+:27358_172884-344768-R+\n\t\t* fwd_2/2.k61\t2000 bp\tA\n\t\t* rev_1/2.k61\t2000 bp\tA"
+with open(commandOut) as out:
+    if expectedString in out.read():
+        print("MTG-Link DBG w/ ref: Pass")
+    else:
+        print("MTG-Link DBG w/ ref: Fail")
+
+subprocess.run("rm LOGMTGLinkDBGRunIntoRef", shell=True)
+subprocess.run("rm ERRLOGMTGLinkDBGRunIntoRef", shell=True)
+subprocess.run("rm -rf test_run/gapsIntoScaffolds/results_MTGLink_DBG_withRef", shell=True)
+
 
 # ### MTG-Link IRO with reference
 # /usr/bin/time -v ../mtglink.py IRO -gfa test_run/gapsIntoScaffolds/test.gfa -bam test_run/gapsIntoScaffolds/test.bam -fastq test_run/gapsIntoScaffolds/reads.sorted.fastq.gz -index test_run/gapsIntoScaffolds/barcoded.bci -s 20 -t 3 -refDir test_run/gapsIntoScaffolds -out test_run/gapsIntoScaffolds/results_MTGLink_IRO_withRef > LOGMTGLinkIRORunIntoRef 2> ERRLOGMTGLinkIRORunIntoRef  
@@ -182,18 +191,29 @@ subprocess.run("rm -rf test_run/gapsBetweenScaffolds/results_MTGLink_DBG", shell
 # subprocess.run("rm -rf test_run/gapsBetweenScaffolds/results_MTGLink_IRO", shell=True)
 
 
-# ### MTG-Link DBG with reference
-# /usr/bin/time -v ../mtglink.py DBG -gfa test_run/gapsBetweenScaffolds/test.gfa -bam test_run/gapsBetweenScaffolds/test.bam -fastq test_run/gapsBetweenScaffolds/reads.sorted.fastq.gz -index test_run/gapsBetweenScaffolds/barcoded.bci -k 61 51 41 31 21 -t 3 -refDir test_run/gapsBetweenScaffolds -out test_run/gapsBetweenScaffolds/results_MTGLink_DBG_withRef > LOGMTGLinkDBGRunBetweenRef 2> ERRLOGMTGLinkDBGRunBetweenRef  
-# if (awk '/\t\* 68-1+:68-2+\n\t\t\* fwd\t2000 bp\tA\n\t\t\* rev\t2000 bp\tA\n\t\* 26939-1+:26939-2+\n\t\t\* fwd\t2004 bp\tB\n\t\t\* rev\t2004 bp\tB/' LOGMTGLinkDBGRunBetweenRef) && (awk '/The gap 27292-1+_27292-2+ was not successfully gap-filled/' LOGMTGLinkDBGRunBetweenRef)
-# then
-#     echo "MTG-Link DBG w/ ref: Pass"
-# else
-#     echo "MTG-Link DBG w/ ref: Fail"
-# fi
+### MTG-Link DBG with reference
+command = ["/usr/bin/time", "-v", "../mtglink.py", "DBG", \
+            "-gfa", "test_run/gapsBetweenScaffolds/test.gfa", "-bam", "test_run/gapsBetweenScaffolds/test.bam", \
+            "-fastq", "test_run/gapsBetweenScaffolds/reads.sorted.fastq.gz", "-index", "test_run/gapsBetweenScaffolds/barcoded.bci", \
+            "-k", "61", "51", "41", "31", "21", "-t", "4", \
+            "-refDir", "test_run/gapsBetweenScaffolds/", \
+            "-out", "test_run/gapsBetweenScaffolds/results_MTGLink_DBG_withRef"]
+commandOut = "LOGMTGLinkDBGRunBetweenRef"
+commandLog = "ERRLOGMTGLinkDBGRunBetweenRef"
+with open(commandOut, "a") as out, open(commandLog, "a") as log:
+    subprocess.run(command, stdout=out, stderr=log)
 
-# rm LOGMTGLinkDBGRunBetweenRef
-# rm ERRLOGMTGLinkDBGRunBetweenRef
-# rm -rf test_run/gapsBetweenScaffolds/results_MTGLink_DBG_withRef
+expectedString = "In total, 2 targets were successfully assembled:\n\t* 68-1+:68-2+\n\t\t* fwd_1/1.k61\t2000 bp\tA\n\t\t* rev_1/1.k61\t2000 bp\tA\n\t* 26939-1+:26939-2+\n\t\t* fwd_1/1.k51\t2004 bp\tB\n\t\t* rev_1/1.k51\t2004 bp\tB"
+with open(commandOut) as out:
+    if expectedString in out.read():
+        print("MTG-Link DBG w/o ref: Pass")
+    else:
+        print("MTG-Link DBG w/o ref: Fail")
+
+subprocess.run("rm LOGMTGLinkDBGRunBetweenRef", shell=True)
+subprocess.run("rm ERRLOGMTGLinkDBGRunBetweenRef", shell=True)
+subprocess.run("rm -rf test_run/gapsBetweenScaffolds/results_MTGLink_DBG_withRef", shell=True)
+
 
 # ### MTG-Link IRO with reference
 # /usr/bin/time -v ../mtglink.py IRO -gfa test_run/gapsBetweenScaffolds/test.gfa -bam test_run/gapsBetweenScaffolds/test.bam -fastq test_run/gapsBetweenScaffolds/reads.sorted.fastq.gz -index test_run/gapsBetweenScaffolds/barcoded.bci -s 20 -t 3 -refDir test_run/gapsBetweenScaffolds -out test_run/gapsBetweenScaffolds/results_MTGLink_IRO_withRef > LOGMTGLinkIRORunBetweenRef 2> ERRLOGMTGLinkIRORunBetweenRef  
