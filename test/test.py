9d1bb9c754d3e49fc6d65a2d4afb66c8ea037abd
Anne Guichard <anne.guichard@irisa.fr>
2022-09-27 16:12:45 +0200

    refactor(test/test.py): rm the tests with the '-refDir' parameter

--- test/test.py
+++ test/test.py
@@ -59,7 +59,7 @@ if passed:
 
 print("TEST2: RUN: GAPS INTO SCAFFOLDS")
 
-### MTG-Link DBG without reference
+### MTG-Link DBG
 command = ["/usr/bin/time", "-v", "../mtglink.py", "DBG", \
             "-gfa", "test_run/gapsIntoScaffolds/test.gfa", "-bam", "test_run/gapsIntoScaffolds/test.bam", \
             "-fastq", "test_run/gapsIntoScaffolds/reads.sorted.fastq.gz", "-index", "test_run/gapsIntoScaffolds/barcoded.bci", \
@@ -74,10 +74,10 @@ expectedString = "In total, 2 targets were successfully assembled:\n\t* 68_0-222
 with open(commandOut) as out:
     if expectedString in out.read():
         passed = True
-        print("MTG-Link DBG w/o ref: Pass")
+        print("MTG-Link DBG: Pass")
     else:
         passed = False
-        print("MTG-Link DBG w/o ref: Fail")
+        print("MTG-Link DBG: Fail")
 
 # # We need these files for the test with the '-bxuDir' arg, so do not remove it now. 
 # subprocess.run("rm LOGMTGLinkDBGRunInto", shell=True)
@@ -85,7 +85,7 @@ with open(commandOut) as out:
 # subprocess.run("rm -rf test_run/gapsIntoScaffolds/results_MTGLink_DBG", shell=True)
 
 
-# ### MTG-Link IRO without reference
+# ### MTG-Link IRO
 # command = ["/usr/bin/time", "-v", "../mtglink.py", "IRO", \
 #             "-gfa", "test_run/gapsIntoScaffolds/test.gfa", "-bam", "test_run/gapsIntoScaffolds/test.bam", \
 #             "-fastq", "test_run/gapsIntoScaffolds/reads.sorted.fastq.gz", "-index", "test_run/gapsIntoScaffolds/barcoded.bci", \
@@ -99,54 +99,14 @@ with open(commandOut) as out:
 # expectedString = "In total, 2 targets were successfully assembled:\n\t* 68_0-222132-L+:68_223132-445265-R+\n\t\t* fwd.k0\t1998 bp\tAB\n\t* 26939_0-464805-L+:26939_465805-930610-R+\n\t\t* fwd.k0\t2014 bp\tAA"
 # with open(commandOut) as out:
 #     if expectedString in out.read():
-#         print("MTG-Link IRO w/o ref: Pass")
+#         print("MTG-Link IRO: Pass")
 #     else:
-#         print("MTG-Link IRO w/o ref: Fail")
+#         print("MTG-Link IRO: Fail")
 
 # subprocess.run("rm LOGMTGLinkIRORunInto", shell=True)
 # subprocess.run("rm ERRLOGMTGLinkIRORunInto", shell=True)
 # subprocess.run("rm -rf test_run/gapsIntoScaffolds/results_MTGLink_IRO", shell=True)
 
-
-### MTG-Link DBG with reference
-command = ["/usr/bin/time", "-v", "../mtglink.py", "DBG", \
-            "-gfa", "test_run/gapsIntoScaffolds/test.gfa", "-bam", "test_run/gapsIntoScaffolds/test.bam", \
-            "-fastq", "test_run/gapsIntoScaffolds/reads.sorted.fastq.gz", "-index", "test_run/gapsIntoScaffolds/barcoded.bci", \
-            "-k", "61", "51", "41", "31", "21", "-t", "4", \
-            "-refDir", "test_run/gapsIntoScaffolds/", \
-            "-out", "test_run/gapsIntoScaffolds/results_MTGLink_DBG_withRef"]
-commandOut = "LOGMTGLinkDBGRunIntoRef"
-commandLog = "ERRLOGMTGLinkDBGRunIntoRef"
-with open(commandOut, "a") as out, open(commandLog, "a") as log:
-    subprocess.run(command, stdout=out, stderr=log)
-
-expectedString = "In total, 3 targets were successfully assembled:\n\t* 68_0-222132-L+:68_223132-445265-R+\n\t\t* fwd_1/1.k61\t2000 bp\tA\n\t\t* rev_1/1.k61\t2000 bp\tA\n\t* 26939_0-464805-L+:26939_465805-930610-R+\n\t\t* fwd_1/1.k51\t2004 bp\tB\n\t\t* rev_1/1.k51\t2004 bp\tB\n\t* 27358_0-171884-L+:27358_172884-344768-R+\n\t\t* fwd_2/2.k61\t2000 bp\tA\n\t\t* rev_1/2.k61\t2000 bp\tA"
-with open(commandOut) as out:
-    if expectedString in out.read():
-        passed = True
-        print("MTG-Link DBG w/ ref: Pass")
-    else:
-        passed = False
-        print("MTG-Link DBG w/ ref: Fail")
-
-if passed:
-    subprocess.run("rm LOGMTGLinkDBGRunIntoRef", shell=True)
-    subprocess.run("rm ERRLOGMTGLinkDBGRunIntoRef", shell=True)
-    subprocess.run("rm -rf test_run/gapsIntoScaffolds/results_MTGLink_DBG_withRef", shell=True)
-
-
-# ### MTG-Link IRO with reference
-# /usr/bin/time -v ../mtglink.py IRO -gfa test_run/gapsIntoScaffolds/test.gfa -bam test_run/gapsIntoScaffolds/test.bam -fastq test_run/gapsIntoScaffolds/reads.sorted.fastq.gz -index test_run/gapsIntoScaffolds/barcoded.bci -s 20 -t 3 -refDir test_run/gapsIntoScaffolds -out test_run/gapsIntoScaffolds/results_MTGLink_IRO_withRef > LOGMTGLinkIRORunIntoRef 2> ERRLOGMTGLinkIRORunIntoRef  
-# if (awk '/\t\* 68_0-222132-L+:68_223132-445265-R+\n\t\t\* fwd\t1998 bp\tB\n\t\* 26939_0-464805-L+:26939_465805-930610-R+\n\t\t\* fwd\t2014 bp\tB/' LOGMTGLinkIRORunIntoRef) && (awk '/The gap 27292_0-183358-L+_27292_184358-367715-R+ was not successfully gap-filled/' LOGMTGLinkIRORunIntoRef)
-# then
-#     echo "MTG-Link IRO w/ ref: Pass"
-# else
-#     echo "MTG-Link IRO w/ ref: Fail"
-# fi
-
-# rm LOGMTGLinkIRORunIntoRef
-# rm ERRLOGMTGLinkIRORunIntoRef
-# rm -rf test_run/gapsIntoScaffolds/results_MTGLink_IRO_withRef
 
 
 
@@ -154,7 +114,7 @@ if passed:
 
 print("TEST3: RUN: GAPS BETWEEN SCAFFOLDS")
 
-### MTG-Link DBG without reference
+### MTG-Link DBG
 command = ["/usr/bin/time", "-v", "../mtglink.py", "DBG", \
             "-gfa", "test_run/gapsBetweenScaffolds/test.gfa", "-bam", "test_run/gapsBetweenScaffolds/test.bam", \
             "-fastq", "test_run/gapsBetweenScaffolds/reads.sorted.fastq.gz", "-index", "test_run/gapsBetweenScaffolds/barcoded.bci", \
@@ -169,10 +129,10 @@ expectedString = "In total, 2 targets were successfully assembled:\n\t* 68-1+:68
 with open(commandOut) as out:
     if expectedString in out.read():
         passed = True
-        print("MTG-Link DBG w/o ref: Pass")
+        print("MTG-Link DBG: Pass")
     else:
         passed = False
-        print("MTG-Link DBG w/o ref: Fail")
+        print("MTG-Link DBG: Fail")
 
 if passed:
     subprocess.run("rm LOGMTGLinkDBGRunBetween", shell=True)
@@ -180,7 +140,7 @@ if passed:
     subprocess.run("rm -rf test_run/gapsBetweenScaffolds/results_MTGLink_DBG", shell=True)
 
 
-# ### MTG-Link IRO without reference
+# ### MTG-Link IRO
 # command = ["/usr/bin/time", "-v", "../mtglink.py", "IRO", \
 #             "-gfa", "test_run/gapsBetweenScaffolds/test.gfa", "-bam", "test_run/gapsBetweenScaffolds/test.bam", \
 #             "-fastq", "test_run/gapsBetweenScaffolds/reads.sorted.fastq.gz", "-index", "test_run/gapsBetweenScaffolds/barcoded.bci", \
@@ -194,54 +154,14 @@ if passed:
 # expectedString = "In total, 2 targets were successfully assembled:\n\t* 68-1+:68-2+\n\t\t* fwd.k0\t1998 bp\tAB\n\t* 26939-1+:26939-2+\n\t\t* fwd.k0\t2014 bp\tAA"
 # with open(commandOut) as out:
 #     if expectedString in out.read():
-#         print("MTG-Link IRO w/o ref: Pass")
+#         print("MTG-Link IRO: Pass")
 #     else:
-#         print("MTG-Link IRO w/o ref: Fail")
+#         print("MTG-Link IRO: Fail")
 
 # subprocess.run("rm LOGMTGLinkIRORunBetween", shell=True)
 # subprocess.run("rm ERRLOGMTGLinkIRORunBetween", shell=True)
 # subprocess.run("rm -rf test_run/gapsBetweenScaffolds/results_MTGLink_IRO", shell=True)
 
-
-### MTG-Link DBG with reference
-command = ["/usr/bin/time", "-v", "../mtglink.py", "DBG", \
-            "-gfa", "test_run/gapsBetweenScaffolds/test.gfa", "-bam", "test_run/gapsBetweenScaffolds/test.bam", \
-            "-fastq", "test_run/gapsBetweenScaffolds/reads.sorted.fastq.gz", "-index", "test_run/gapsBetweenScaffolds/barcoded.bci", \
-            "-k", "61", "51", "41", "31", "21", "-t", "4", \
-            "-refDir", "test_run/gapsBetweenScaffolds/", \
-            "-out", "test_run/gapsBetweenScaffolds/results_MTGLink_DBG_withRef"]
-commandOut = "LOGMTGLinkDBGRunBetweenRef"
-commandLog = "ERRLOGMTGLinkDBGRunBetweenRef"
-with open(commandOut, "a") as out, open(commandLog, "a") as log:
-    subprocess.run(command, stdout=out, stderr=log)
-
-expectedString = "In total, 2 targets were successfully assembled:\n\t* 68-1+:68-2+\n\t\t* fwd_1/1.k61\t2000 bp\tA\n\t\t* rev_1/1.k61\t2000 bp\tA\n\t* 26939-1+:26939-2+\n\t\t* fwd_1/1.k51\t2004 bp\tB\n\t\t* rev_1/1.k51\t2004 bp\tB"
-with open(commandOut) as out:
-    if expectedString in out.read():
-        passed = True
-        print("MTG-Link DBG w/o ref: Pass")
-    else:
-        passed = False
-        print("MTG-Link DBG w/o ref: Fail")
-
-if passed:
-    subprocess.run("rm LOGMTGLinkDBGRunBetweenRef", shell=True)
-    subprocess.run("rm ERRLOGMTGLinkDBGRunBetweenRef", shell=True)
-    subprocess.run("rm -rf test_run/gapsBetweenScaffolds/results_MTGLink_DBG_withRef", shell=True)
-
-
-# ### MTG-Link IRO with reference
-# /usr/bin/time -v ../mtglink.py IRO -gfa test_run/gapsBetweenScaffolds/test.gfa -bam test_run/gapsBetweenScaffolds/test.bam -fastq test_run/gapsBetweenScaffolds/reads.sorted.fastq.gz -index test_run/gapsBetweenScaffolds/barcoded.bci -s 20 -t 3 -refDir test_run/gapsBetweenScaffolds -out test_run/gapsBetweenScaffolds/results_MTGLink_IRO_withRef > LOGMTGLinkIRORunBetweenRef 2> ERRLOGMTGLinkIRORunBetweenRef  
-# if (awk '/\t\* 68-1+:68-2+\n\t\t\* fwd\t1998 bp\tB\n\t\* 26939-1+:26939-2+\n\t\t\* fwd\t2014 bp\tB/' LOGMTGLinkIRORunBetweenRef) && (awk '/The gap 27292-1+_27292-2+ was not successfully gap-filled/' LOGMTGLinkIRORunBetweenRef)
-# then
-#     echo "MTG-Link IRO w/ ref: Pass"
-# else
-#     echo "MTG-Link IRO w/ ref: Fail"
-# fi
-
-# rm LOGMTGLinkIRORunBetweenRef
-# rm ERRLOGMTGLinkIRORunBetweenRef
-# rm -rf test_run/gapsBetweenScaffolds/results_MTGLink_IRO_withRef
 
 
 
@@ -249,7 +169,7 @@ if passed:
 
 print("TEST4: RUN DBG -bxuDir: GAPS INTO SCAFFOLDS")
 
-### MTG-Link DBG -bxuDir without reference 
+### MTG-Link DBG -bxuDir
 command = ["/usr/bin/time", "-v", "../mtglink.py", "DBG", \
             "-gfa", "test_run/gapsIntoScaffolds/test.gfa", "-bam", "test_run/gapsIntoScaffolds/test.bam", \
             "-fastq", "test_run/gapsIntoScaffolds/reads.sorted.fastq.gz", "-index", "test_run/gapsIntoScaffolds/barcoded.bci", \
@@ -265,10 +185,10 @@ expectedString = "In total, 2 targets were successfully assembled:\n\t* 68_0-222
 with open(commandOut) as out:
     if expectedString in out.read():
         passed = True
-        print("MTG-Link DBG -bxuDir w/o ref: Pass")
+        print("MTG-Link DBG -bxuDir: Pass")
     else:
         passed = False
-        print("MTG-Link DBG -bxuDir w/o ref: Fail")
+        print("MTG-Link DBG -bxuDir: Fail")
 
 if passed:
     subprocess.run("rm LOGMTGLinkDBGRunInto", shell=True)
@@ -284,7 +204,7 @@ if passed:
 
 print("TEST5: RUN DBG --multiple: GAPS INTO SCAFFOLDS")
 
-### MTG-Link DBG --multiple without reference 
+### MTG-Link DBG --multiple
 command = ["/usr/bin/time", "-v", "../mtglink.py", "DBG", \
             "-gfa", "test_run/gapsIntoScaffolds/test.gfa", "-bam", "test_run/gapsIntoScaffolds/test.bam", \
             "-fastq", "test_run/gapsIntoScaffolds/reads.sorted.fastq.gz", "-index", "test_run/gapsIntoScaffolds/barcoded.bci", \
@@ -299,10 +219,10 @@ expectedString = "In total, 3 targets were successfully assembled:\n\t* 68_0-222
 with open(commandOut) as out:
     if expectedString in out.read():
         passed = True
-        print("MTG-Link DBG --multiple w/o ref: Pass")
+        print("MTG-Link DBG --multiple: Pass")
     else:
         passed = False
-        print("MTG-Link DBG --multiple w/o ref: Fail")
+        print("MTG-Link DBG --multiple: Fail")
 
 if passed:
     subprocess.run("rm LOGMTGLinkDBGRunIntoMultiple", shell=True)
@@ -315,7 +235,7 @@ if passed:
 
 print("TEST6: RUN DBG --force: GAPS INTO SCAFFOLDS")
 
-### MTG-Link DBG --force without reference 
+### MTG-Link DBG --force
 command = ["/usr/bin/time", "-v", "../mtglink.py", "DBG", \
             "-gfa", "test_run/gapsIntoScaffolds/test.gfa", "-bam", "test_run/gapsIntoScaffolds/test.bam", \
             "-fastq", "test_run/gapsIntoScaffolds/reads.sorted.fastq.gz", "-index", "test_run/gapsIntoScaffolds/barcoded.bci", \
@@ -330,10 +250,10 @@ expectedString = "In total, 3 targets were successfully assembled:\n\t* 68_0-222
 with open(commandOut) as out:
     if expectedString in out.read():
         passed = True
-        print("MTG-Link DBG --force w/o ref: Pass")
+        print("MTG-Link DBG --force: Pass")
     else:
         passed = False
-        print("MTG-Link DBG --force w/o ref: Fail")
+        print("MTG-Link DBG --force: Fail")
 
 if passed:
     subprocess.run("rm LOGMTGLinkDBGRunIntoForce", shell=True)
@@ -346,7 +266,7 @@ if passed:
 
 print("TEST7: RUN DBG --force -l -m: GAPS INTO SCAFFOLDS")
 
-### MTG-Link DBG --force -l -m without reference 
+### MTG-Link DBG --force -l -m
 command = ["/usr/bin/time", "-v", "../mtglink.py", "DBG", \
             "-gfa", "test_run/gapsIntoScaffolds/test.gfa", "-bam", "test_run/gapsIntoScaffolds/test.bam", \
             "-fastq", "test_run/gapsIntoScaffolds/reads.sorted.fastq.gz", "-index", "test_run/gapsIntoScaffolds/barcoded.bci", \
@@ -361,10 +281,10 @@ expectedString = "In total, 3 targets were successfully assembled:\n\t* 68_0-222
 with open(commandOut) as out:
     if expectedString in out.read():
         passed = True
-        print("MTG-Link DBG --force -l -m w/o ref: Pass")
+        print("MTG-Link DBG --force -l -m: Pass")
     else:
         passed = False
-        print("MTG-Link DBG --force -l -m w/o ref: Fail")
+        print("MTG-Link DBG --force -l -m: Fail")
 
 if passed:
     subprocess.run("rm LOGMTGLinkDBGRunIntoForce_lm", shell=True)
